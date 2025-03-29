e34f76927e80aa03a02b398ed8fda1fe8bf8c93e
Cindy Su <su000204@umn.edu>
2025-03-29 01:09:34 -0500

    javadoc comments for Ballot.java

--- Project1/src/main/Ballot.java
+++ Project1/src/main/Ballot.java
@@ -1,10 +1,25 @@
 package main;
+
+/**
+ * Abstract class representing Ballot for election. 
+ */
 abstract public class Ballot {
+
     private int[] vote;
 
+    /**
+     * Constructor for Ballot.
+     * 
+     * @param vote Array of integers representing vote
+     */
     public Ballot(int[] vote) {
         this.vote = vote;
     }
 
+    /**
+     * Abstract method to get vote.
+     * 
+     * @return an array of integers representing vote
+     */
     abstract public int[] getVote();
 }
