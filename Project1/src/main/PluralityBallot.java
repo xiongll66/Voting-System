da567532b02d09028abca52492a74fa623643ef1
Elsdon Chang <chan2242@umn.edu>
2025-03-29 11:36:11 -0500

    removed private vote and getVote method to inherit from Ballot

--- Project1/src/main/PluralityBallot.java
+++ Project1/src/main/PluralityBallot.java
@@ -1,12 +1,6 @@
 package main;
 public class PluralityBallot extends Ballot {
-    private int[] vote;
-
     public PluralityBallot(int[] vote) {
         super(vote);
-    }
-    
-    public int[] getVote() {
-        return this.vote;
     }
 }
