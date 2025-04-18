fee66572a70490519b2ecc569dcc5fd0e6529466
Elsdon Chang <chan2242@umn.edu>
2025-04-18 16:20:38 -0500

    added Header class to store election info from ballot file header

---
+++ Project2/src/main/Header.java
@@ -0,0 +1,25 @@
+package main;
+
+public class Header {
+    private String electionType;
+    private int numSeats;
+    private String[] candidates;
+
+    public Header(String electionType, int numSeats, String[] candidates) {
+        this.electionType = electionType;
+        this.numSeats = numSeats;
+        this.candidates = candidates;
+    }
+
+    public String getElectionType() {
+        return electionType;
+    }
+
+    public int getNumSeats() {
+        return numSeats;
+    }
+    
+    public String[] getCandidates() {
+        return candidates;
+    }
+}
