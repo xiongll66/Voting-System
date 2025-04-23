a7aa71e5acc625f1c54fc37887476cac532f3a89
Cindy Su <su000204@umn.edu>
2025-04-22 20:58:21 -0500

    created MVBallot class

---
+++ Project2/src/main/MVBallot.java
@@ -0,0 +1,16 @@
+package main;
+
+/**
+ * Class that extends Ballot to represent a MV Ballot.
+ */
+public class MVBallot extends Ballot{
+
+    /**
+     * Constructor for MVBallot.
+     * 
+     * @param vote integer array containing the vote
+     */
+    public MVBallot(int[] vote) {
+        super(vote);
+    }
+}
